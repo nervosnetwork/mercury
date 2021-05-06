a1f5d32464a50b8926c052dc4436c6a58918cf82
KaoImin <kaoimin@qq.com>
2021-05-06 20:43:34 +0800

    refactor: rce validator extension

--- src/extensions/rce_validator/mod.rs
+++ src/extensions/rce_validator/mod.rs
@@ -37,47 +37,17 @@ where
         let mut rollback_deletions = HashSet::new();
 
         let mut batch = self.store.batch()?;
-        for tx in block.data().transactions() {
-            for (i, output) in tx.raw().outputs().into_iter().enumerate() {
-                if let Some(type_script) = output.type_().to_opt() {
-                    let rce_config = self.config.get(RCE).expect("empty config");
-
-                    if type_script.code_hash() == rce_config.script.code_hash()
-                        && type_script.hash_type() == rce_config.script.hash_type()
-                    {
-                        // TODO: do we need to purge unused scripts?
-                        let script_hash = type_script.calc_script_hash();
-                        batch.put_kv(Key::ScriptHash(&script_hash), Value::Script(&type_script))?;
-
-                        let witness = tx.witnesses().get(i).expect("invalid witness");
-                        let witness_args = packed::WitnessArgs::from_slice(&witness.raw_data())
-                            .expect("invalid witness format");
-                        let output_type = witness_args
-                            .output_type()
-                            .to_opt()
-                            .expect("invalid witness output type");
-                        let smt_update = SmtUpdate::from_slice(&output_type.raw_data())
-                            .expect("invalid smt update");
-
-                        for item in smt_update.update() {
-                            let presence = u8::from(item.values()) & 0xF == 0x1;
-                            let script_args = type_script.args().raw_data();
-                            let address = item.key();
-                            let key = Key::Address(&script_args, &address).into_vec();
-                            let old_presence = self.store.exists(key.clone())?;
-                            if presence {
-                                batch.put_kv(key.clone(), vec![0x1])?;
-                                if !old_presence {
-                                    rollback_deletions.insert(Bytes::from(key));
-                                }
-                            } else {
-                                batch.delete(key.clone())?;
-                                if old_presence {
-                                    rollback_insertions.insert(Bytes::from(key));
-                                }
-                            }
-                        }
-                    }
+        for tx in block.data().transactions().into_iter() {
+            for (idx, output) in tx.raw().outputs().into_iter().enumerate() {
+                if self.is_rce_cell(&output)? {
+                    self.rce_process(
+                        idx,
+                        &mut batch,
+                        &output.type_().to_opt().unwrap(),
+                        &tx,
+                        &mut rollback_deletions,
+                        &mut rollback_insertions,
+                    )?;
                 }
             }
         }
@@ -115,6 +85,7 @@ where
 
         batch.delete(block_key)?;
         batch.commit()?;
+        
         Ok(())
     }
 
@@ -128,10 +99,12 @@ where
             let prune_to_block = tip_number - keep_num;
             let mut batch = self.store.batch()?;
             let block_key_prefix = vec![KeyPrefix::Block as u8];
+
             let iter = self
                 .store
                 .iter(&block_key_prefix, IteratorDirection::Forward)?
                 .take_while(|(key, _value)| key.starts_with(&block_key_prefix));
+
             for (_block_number, key) in iter
                 .map(|(key, _value)| {
                     (
@@ -145,8 +118,91 @@ where
             {
                 batch.delete(key)?;
             }
+
             batch.commit()?;
         }
+
         Ok(())
+    }
+}
+
+impl<S: Store> RceValidatorExtension<S> {
+    fn rce_process(
+        &self,
+        index: usize,
+        batch: &mut <S as Store>::Batch,
+        type_script: &packed::Script,
+        tx: &packed::Transaction,
+        rollback_deletions: &mut HashSet<Bytes>,
+        rollback_insertions: &mut HashSet<Bytes>,
+    ) -> Result<()> {
+        // TODO: do we need to purge unused scripts?
+        let script_hash = type_script.calc_script_hash();
+        batch.put_kv(Key::ScriptHash(&script_hash), Value::Script(&type_script))?;
+
+        let witness = tx.witnesses().get(index).expect("invalid witness");
+        let witness_args =
+            packed::WitnessArgs::from_slice(&witness.raw_data()).expect("invalid witness format");
+        let output_type = witness_args
+            .output_type()
+            .to_opt()
+            .expect("invalid witness output type");
+        let smt_update =
+            SmtUpdate::from_slice(&output_type.raw_data()).expect("invalid smt update");
+
+        for item in smt_update.update().into_iter() {
+            self.update_smt(
+                &item,
+                type_script,
+                batch,
+                rollback_deletions,
+                rollback_insertions,
+            )?;
+        }
+
+        Ok(())
+    }
+
+    fn update_smt(
+        &self,
+        item: &generated::xudt_rce::SmtUpdateItem,
+        type_script: &packed::Script,
+        batch: &mut <S as Store>::Batch,
+        rollback_deletions: &mut HashSet<Bytes>,
+        rollback_insertions: &mut HashSet<Bytes>,
+    ) -> Result<(), anyhow::Error> {
+        let presence = u8::from(item.values()) & 0xF == 0x1;
+        let script_args = type_script.args().raw_data();
+        let address = item.key();
+        let key = Key::Address(&script_args, &address).into_vec();
+        let old_presence = self.store.exists(key.clone())?;
+
+        if presence {
+            batch.put_kv(key.clone(), vec![0x1])?;
+            if !old_presence {
+                rollback_deletions.insert(Bytes::from(key));
+            }
+        } else {
+            batch.delete(key.clone())?;
+            if old_presence {
+                rollback_insertions.insert(Bytes::from(key));
+            }
+        }
+
+        Ok(())
+    }
+
+    fn is_rce_cell(&self, cell: &packed::CellOutput) -> Result<bool> {
+        if let Some(type_script) = cell.type_().to_opt() {
+            let rce_config = self.config.get(RCE).expect("empty config");
+
+            if type_script.code_hash() == rce_config.script.code_hash()
+                && type_script.hash_type() == rce_config.script.hash_type()
+            {
+                return Ok(true);
+            }
+        }
+
+        Ok(false)
     }
 }
