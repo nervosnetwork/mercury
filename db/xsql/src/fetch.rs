011c7f2cd69c7c9d22004b03b3ed03f8f11150b3
EthanYuan <yuanchengxing@hotmail.com>
2021-08-17 18:07:59 +0800

    extract build method.

--- db/xsql/src/fetch.rs
+++ db/xsql/src/fetch.rs
@@ -1,16 +1,21 @@
-use crate::table::{BlockTable, UncleRelationshipTable};
+use crate::table::{BlockTable, CellTable, TransactionTable, UncleRelationshipTable};
 use crate::{error::DBError, DBAdapter, XSQLPool};
 
 use common::{anyhow::Result, utils};
 
 use bson::Binary;
+use ckb_types::bytes::Bytes;
 use ckb_types::core::{
     BlockBuilder, BlockNumber, BlockView, EpochNumberWithFraction, HeaderBuilder, HeaderView,
-    TransactionView, UncleBlockView,
+    TransactionBuilder, TransactionView, UncleBlockView,
 };
-use ckb_types::packed::{ProposalShortIdVec, UncleBlockBuilder};
+use ckb_types::packed::{
+    Byte32, CellDep, CellInput, CellOutput, ProposalShortIdVec, UncleBlockBuilder,
+};
 use ckb_types::{packed, prelude::*, H256};
 use rbatis::crud::CRUD;
+
+use std::collections::HashMap;
 
 pub type BsonBytes = Binary;
 
@@ -65,8 +70,55 @@ impl<T: DBAdapter> XSQLPool<T> {
         Ok(uncles)
     }
 
-    async fn get_transactions(&self, _block: &BlockTable) -> Result<Vec<TransactionView>> {
-        todo!()
+    async fn get_transactions(&self, block: &BlockTable) -> Result<Vec<TransactionView>> {
+        let txs = self.query_transactions(&block.block_hash).await?;
+        let tx_hashes: Vec<Binary> = txs.iter().map(|tx| tx.tx_hash.clone()).collect();
+        let cells: Vec<CellTable> = self.query_txs_cells(&tx_hashes).await?;
+
+        let mut txs_input_cells: HashMap<Vec<u8>, Vec<CellTable>> = txs
+            .iter()
+            .map(|tx| (tx.tx_hash.bytes.clone(), vec![]))
+            .collect();
+        let mut txs_output_cells: HashMap<Vec<u8>, Vec<CellTable>> = txs
+            .iter()
+            .map(|tx| (tx.tx_hash.bytes.clone(), vec![]))
+            .collect();
+        for cell in cells {
+            match cell.input_index {
+                Some(_) => {
+                    if let Some(set) = txs_input_cells.get_mut(&cell.tx_hash.bytes) {
+                        (*set).push(cell)
+                    }
+                }
+                None => {
+                    if let Some(set) = txs_output_cells.get_mut(&cell.tx_hash.bytes) {
+                        (*set).push(cell)
+                    }
+                }
+            }
+        }
+
+        let tx_views = txs
+            .into_iter()
+            .map(|tx| {
+                let witness = build_witness(&tx.witnesses.bytes);
+                let header_deps = build_header_deps(&tx.header_deps.bytes);
+                let cell_deps = build_cell_deps(&tx.cell_deps.bytes);
+                let inputs = build_cell_inputs(txs_input_cells.get(&tx.block_hash.bytes));
+                let outputs = build_cell_outputs(txs_output_cells.get(&tx.block_hash.bytes));
+                let outputs_data = build_outputs_data(txs_output_cells.get(&tx.block_hash.bytes));
+                build_transaction_view(
+                    tx.version as u32,
+                    witness,
+                    inputs,
+                    outputs,
+                    outputs_data,
+                    cell_deps,
+                    header_deps,
+                )
+            })
+            .collect();
+        Ok(tx_views)
     }
 
     // TODO: query refactoring
@@ -118,6 +170,22 @@ impl<T: DBAdapter> XSQLPool<T> {
             .fetch_list_by_column("block_hash", &uncle_hashes)
             .await?;
         Ok(uncles)
+    }
+
+    async fn query_transactions(&self, block_hash: &Binary) -> Result<Vec<TransactionTable>> {
+        let txs: Vec<TransactionTable> = self
+            .inner
+            .fetch_list_by_column("block_hash", &[block_hash])
+            .await?;
+        Ok(txs)
+    }
+
+    async fn query_txs_cells(&self, tx_hashes: &[Binary]) -> Result<Vec<CellTable>> {
+        let cells: Vec<CellTable> = self
+            .inner
+            .fetch_list_by_column("tx_hash", &[tx_hashes])
+            .await?;
+        Ok(cells)
     }
 }
 
@@ -181,4 +249,55 @@ fn build_header_view(block: &BlockTable) -> HeaderView {
 
 fn build_proposals(_input: &[u8]) -> ProposalShortIdVec {
     todo!()
+}
+
+fn build_witness(_input: &[u8]) -> Vec<packed::Bytes> {
+    todo!()
+}
+
+fn build_header_deps(_input: &[u8]) -> Vec<Byte32> {
+    todo!()
+}
+
+fn build_cell_deps(_input: &[u8]) -> Vec<CellDep> {
+    todo!()
+}
+
+fn build_cell_inputs(_inputs: Option<&Vec<CellTable>>) -> Vec<CellInput> {
+    todo!()
+}
+
+fn build_cell_outputs(_inputs: Option<&Vec<CellTable>>) -> Vec<CellOutput> {
+    todo!()
+}
+
+fn build_outputs_data(cells: Option<&Vec<CellTable>>) -> Vec<packed::Bytes> {
+    let cells = match cells {
+        Some(cells) => cells,
+        None => return vec![],
+    };
+    cells
+        .into_iter()
+        .map(|cell| Bytes::from(cell.data.bytes.clone()).pack())
+        .collect()
+}
+
+fn build_transaction_view(
+    version: u32,
+    witnesses: Vec<packed::Bytes>,
+    inputs: Vec<CellInput>,
+    outputs: Vec<CellOutput>,
+    outputs_data: Vec<packed::Bytes>,
+    cell_deps: Vec<CellDep>,
+    header_deps: Vec<packed::Byte32>,
+) -> TransactionView {
+    TransactionBuilder::default()
+        .version(version.pack())
+        .witnesses(witnesses)
+        .inputs(inputs)
+        .outputs(outputs)
+        .outputs_data(outputs_data)
+        .cell_deps(cell_deps)
+        .header_deps(header_deps)
+        .build()
 }
