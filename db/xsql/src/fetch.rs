7bfe020d30c4e18cb19ce3a8792a569748e06524
Chengxing Yuan <yuanchengxing@hotmail.com>
2021-08-19 13:35:53 +0800

    feat(DB): impl db interface get_block (#107)
    * refactoring.
    
    * refactoring: extract query method.
    
    * extract build method.
    
    * impl build_cell_inputs.
    
    * impl build_outputs_data
    
    * update query_uncles_by_hash and query_transaction.
    
    * update query output cells and input cells.
    
    * fix clippy warning.
    
    Co-authored-by: Eason Gao <kaoimin@qq.com>

--- db/xsql/src/fetch.rs
+++ db/xsql/src/fetch.rs
@@ -1,71 +1,148 @@
-use crate::table::{BlockTable, UncleRelationshipTable};
-use crate::{error::DBError, DBAdapter, XSQLPool};
+use crate::table::{BigDataTable, BlockTable, CellTable, TransactionTable, UncleRelationshipTable};
+use crate::{error::DBError, to_bson_bytes, DBAdapter, XSQLPool};
 
 use common::{anyhow::Result, utils};
 
+use bson::Binary;
+use ckb_types::bytes::Bytes;
 use ckb_types::core::{
     BlockBuilder, BlockNumber, BlockView, EpochNumberWithFraction, HeaderBuilder, HeaderView,
-    UncleBlockView,
+    TransactionBuilder, TransactionView, UncleBlockView,
+};
+use ckb_types::packed::{
+    Byte32, Byte32Vec, CellDep, CellInput, CellInputBuilder, CellOutput, CellOutputBuilder,
+    OutPointBuilder, ProposalShortIdVec, UncleBlockBuilder,
 };
 use ckb_types::{packed, prelude::*, H256};
 use rbatis::crud::CRUD;
 
+use std::collections::HashMap;
+
+pub type BsonBytes = Binary;
+
 impl<T: DBAdapter> XSQLPool<T> {
     pub async fn get_block_by_number(&self, block_number: BlockNumber) -> Result<BlockView> {
-        let block: Option<BlockTable> = self
-            .inner
-            .fetch_by_column("block_number", &block_number)
-            .await?;
-        let block = match block {
-            Some(block) => block,
-            None => return Err(DBError::WrongHeight.into()),
-        };
+        let block = self.query_block_by_number(block_number).await?;
         self.get_block_view(&block).await
     }
 
     pub async fn get_block_by_hash(&self, block_hash: H256) -> Result<BlockView> {
-        let block: Option<BlockTable> = self
-            .inner
-            .fetch_by_column("block_hash", &block_hash)
-            .await?;
-        let block = match block {
-            Some(block) => block,
-            None => return Err(DBError::CannotFind.into()),
-        };
+        let block = self.query_block_by_hash(block_hash).await?;
         self.get_block_view(&block).await
     }
 
-    // Todo: Need refactor
     pub async fn get_tip_block(&self) -> Result<BlockView> {
-        let wrapper = self.wrapper().order_by(false, &["block_number"]).limit(1);
-        let block: Option<BlockTable> = self.inner.fetch_by_wrapper(&wrapper).await?;
-        let block = block.expect("get tip block");
+        let block = self.query_tip_block().await?;
         self.get_block_view(&block).await
     }
 
-    // Todo: Need refactor
     pub async fn get_tip_block_header(&self) -> Result<HeaderView> {
-        let wrapper = self.wrapper().order_by(false, &["block_number"]).limit(1);
-        let block: Option<BlockTable> = self.inner.fetch_by_wrapper(&wrapper).await?;
-        Ok(self.get_header_view(&block.expect("get tip block")))
+        let block = self.query_tip_block().await?;
+        Ok(build_header_view(&block))
     }
 
     pub async fn get_block_header_by_block_hash(&self, block_hash: H256) -> Result<HeaderView> {
-        let block: Option<BlockTable> = self
-            .inner
-            .fetch_by_column("block_hash", &block_hash)
-            .await?;
-        let block = match block {
-            Some(block) => block,
-            None => return Err(DBError::CannotFind.into()),
-        };
-        Ok(self.get_header_view(&block))
+        let block = self.query_block_by_hash(block_hash).await?;
+        Ok(build_header_view(&block))
     }
 
     pub async fn get_block_header_by_block_number(
         &self,
         block_number: BlockNumber,
     ) -> Result<HeaderView> {
+        let block = self.query_block_by_number(block_number).await?;
+        Ok(build_header_view(&block))
+    }
+
+    async fn get_block_view(&self, block: &BlockTable) -> Result<BlockView> {
+        let header = build_header_view(&block);
+        let uncles = self.get_uncle_block_views(&block).await?;
+        let txs = self.get_transactions(&block).await?;
+        let proposals = build_proposals(&block.proposals.bytes);
+        Ok(build_block_view(header, uncles, txs, proposals))
+    }
+
+    async fn get_uncle_block_views(&self, block: &BlockTable) -> Result<Vec<UncleBlockView>> {
+        let uncles = self.query_uncles_by_hash(&block.block_hash).await?;
+        let uncles: Vec<UncleBlockView> = uncles
+            .into_iter()
+            .map(|uncle| build_uncle_block_view(&uncle))
+            .collect();
+        Ok(uncles)
+    }
+
+    async fn get_transactions(&self, block: &BlockTable) -> Result<Vec<TransactionView>> {
+        let txs = self.query_transactions(&block.block_hash).await?;
+        let tx_hashes: Vec<Binary> = txs.iter().map(|tx| tx.tx_hash.clone()).collect();
+        let output_cells: Vec<CellTable> = self.query_txs_output_cells(&tx_hashes).await?;
+        let input_cells: Vec<CellTable> = self.query_txs_input_cells(&tx_hashes).await?;
+
+        let mut txs_output_cells: HashMap<Vec<u8>, Vec<CellTable>> = tx_hashes
+            .iter()
+            .map(|tx_hash| (tx_hash.bytes.clone(), vec![]))
+            .collect();
+        let mut txs_input_cells: HashMap<Vec<u8>, Vec<CellTable>> = tx_hashes
+            .iter()
+            .map(|tx_hash| (tx_hash.bytes.clone(), vec![]))
+            .collect();
+        for cell in output_cells {
+            if let Some(set) = txs_output_cells.get_mut(&cell.tx_hash.bytes) {
+                (*set).push(cell)
+            }
+        }
+        for cell in input_cells {
+            if let Some(set) = txs_input_cells.get_mut(&cell.tx_hash.bytes) {
+                (*set).push(cell)
+            }
+        }
+
+        let tx_views = txs
+            .into_iter()
+            .map(|tx| {
+                let witness = build_witness(&tx.witnesses.bytes);
+                let header_deps = build_header_deps(&tx.header_deps.bytes);
+                let cell_deps = build_cell_deps(&tx.cell_deps.bytes);
+                let inputs = build_cell_inputs(txs_input_cells.get(&tx.tx_hash.bytes));
+                let outputs = build_cell_outputs(txs_output_cells.get(&tx.tx_hash.bytes));
+                let outputs_data = build_outputs_data(txs_output_cells.get(&tx.tx_hash.bytes));
+                build_transaction_view(
+                    tx.version as u32,
+                    witness,
+                    inputs,
+                    outputs,
+                    outputs_data,
+                    cell_deps,
+                    header_deps,
+                )
+            })
+            .collect();
+        Ok(tx_views)
+    }
+
+    // TODO: query refactoring
+    async fn query_tip_block(&self) -> Result<BlockTable> {
+        let wrapper = self.wrapper().order_by(false, &["block_number"]).limit(1);
+        let block: Option<BlockTable> = self.inner.fetch_by_wrapper(&wrapper).await?;
+        let block = match block {
+            Some(block) => block,
+            None => return Err(DBError::CannotFind.into()),
+        };
+        Ok(block)
+    }
+
+    async fn query_block_by_hash(&self, block_hash: H256) -> Result<BlockTable> {
+        let block: Option<BlockTable> = self
+            .inner
+            .fetch_by_column("block_hash", &block_hash)
+            .await?;
+        let block = match block {
+            Some(block) => block,
+            None => return Err(DBError::CannotFind.into()),
+        };
+        Ok(block)
+    }
+
+    async fn query_block_by_number(&self, block_number: BlockNumber) -> Result<BlockTable> {
         let block: Option<BlockTable> = self
             .inner
             .fetch_by_column("block_number", &block_number)
@@ -74,72 +151,221 @@ impl<T: DBAdapter> XSQLPool<T> {
             Some(block) => block,
             None => return Err(DBError::WrongHeight.into()),
         };
-        Ok(self.get_header_view(&block))
+        Ok(block)
     }
 
-    async fn get_block_view(&self, block: &BlockTable) -> Result<BlockView> {
-        let header = self.get_header_view(&block);
-        let uncles = self.get_uncle_block_views(&block).await;
-        // TODO: let txs = get_transactions(&block);
-        // TODO: let proposals = get_proposals(&block);
-        let _block_view = BlockBuilder::default()
-            .header(header)
-            .uncles(uncles)
-            .build();
-        todo!()
-    }
-
-    async fn get_uncle_block_views(&self, block: &BlockTable) -> Vec<UncleBlockView> {
-        let uncles: Vec<UncleRelationshipTable> = self
+    async fn query_uncles_by_hash(&self, block_hash: &Binary) -> Result<Vec<BlockTable>> {
+        let uncle_relationship: Option<UncleRelationshipTable> = self
             .inner
-            .fetch_list_by_column("block_hash", &[(&block.block_hash)])
-            .await
-            .expect("fetch uncle block hash");
-        let uncle_hashes: Vec<Vec<u8>> = uncles
-            .iter()
-            .map(|uncle| uncle.uncle_hashes.bytes.clone())
+            .fetch_by_column("block_hash", &block_hash)
+            .await?;
+        let uncle_relationship = match uncle_relationship {
+            Some(uncle_relationship) => uncle_relationship,
+            None => return Ok(vec![]),
+        };
+        if uncle_relationship.uncle_hashes.bytes == Byte32Vec::default().as_bytes().to_vec() {
+            return Ok(vec![]);
+        }
+        let uncle_hashes =
+            Byte32Vec::new_unchecked(Bytes::from(uncle_relationship.uncle_hashes.bytes));
+        let uncle_hashes: Vec<BsonBytes> = uncle_hashes
+            .into_iter()
+            .map(|hash| to_bson_bytes(hash.as_slice()))
             .collect();
         let uncles: Vec<BlockTable> = self
             .inner
             .fetch_list_by_column("block_hash", &uncle_hashes)
-            .await
-            .expect("fetch uncle block");
-        uncles.into_iter().map(|_| todo!()).collect()
+            .await?;
+        Ok(uncles)
     }
 
-    fn get_header_view(&self, block: &BlockTable) -> HeaderView {
-        HeaderBuilder::default()
-            .number(block.block_number.pack())
-            .parent_hash(
-                packed::Byte32::from_slice(&block.parent_hash.bytes)
-                    .expect("impossible: fail to pack parent_hash"),
-            )
-            .compact_target(block.compact_target.pack())
-            .nonce(utils::decode_nonce(&block.nonce.bytes).pack())
-            .timestamp(block.block_timestamp.pack())
-            .version((block.version as u32).pack())
-            .epoch(
-                EpochNumberWithFraction::new(
-                    block.epoch_number,
-                    block.epoch_block_index as u64,
-                    block.epoch_length as u64,
-                )
-                .number()
-                .pack(),
-            )
-            .dao(packed::Byte32::from_slice(&block.dao.bytes).expect(""))
-            .transactions_root(
-                packed::Byte32::from_slice(&block.transactions_root.bytes)
-                    .expect("impossible: fail to pack transactions_root"),
-            )
-            .proposals_hash(
-                packed::Byte32::from_slice(&block.proposals_hash.bytes)
-                    .expect("impossible: fail to pack proposals_hash"),
-            )
-            .uncles_hash(
-                packed::Byte32::from_slice(&block.uncles_hash.bytes)
-                    .expect("impossible: fail to pack uncles_hash"),
-            )
-            .build()
+    async fn query_transactions(&self, block_hash: &Binary) -> Result<Vec<TransactionTable>> {
+        let w = self
+            .inner
+            .new_wrapper()
+            .eq("block_hash", block_hash)
+            .order_by(true, &["tx_index"]);
+        let txs: Vec<TransactionTable> = self.inner.fetch_list_by_wrapper(&w).await?;
+        Ok(txs)
     }
+
+    async fn query_txs_output_cells(&self, tx_hashes: &[Binary]) -> Result<Vec<CellTable>> {
+        let w = self
+            .inner
+            .new_wrapper()
+            .r#in("tx_hash", &tx_hashes)
+            .order_by(true, &["tx_hash", "output_index"]);
+        let mut cells: Vec<CellTable> = self.inner.fetch_list_by_wrapper(&w).await?;
+        let big_datas: Vec<BigDataTable> = self
+            .inner
+            .fetch_list_by_column("tx_hash", &tx_hashes)
+            .await?;
+        let big_datas: HashMap<(Vec<u8>, u16), Binary> = big_datas
+            .into_iter()
+            .map(|data| ((data.tx_hash.bytes, data.output_index), data.data))
+            .collect();
+        for cell in &mut cells {
+            if !cell.is_data_complete {
+                cell.data = big_datas
+                    .get(&(cell.tx_hash.bytes.clone(), cell.output_index))
+                    .expect("impossible: fail to get big data")
+                    .to_owned();
+            }
+        }
+        Ok(cells)
+    }
+
+    async fn query_txs_input_cells(&self, tx_hashes: &[Binary]) -> Result<Vec<CellTable>> {
+        let w = self
+            .inner
+            .new_wrapper()
+            .r#in("consumed_tx_hash", &tx_hashes)
+            .order_by(true, &["consumed_tx_hash", "input_index"]);
+        let cells: Vec<CellTable> = self.inner.fetch_list_by_wrapper(&w).await?;
+        Ok(cells)
+    }
+}
+
+fn build_block_view(
+    header: HeaderView,
+    uncles: Vec<UncleBlockView>,
+    txs: Vec<TransactionView>,
+    proposals: ProposalShortIdVec,
+) -> BlockView {
+    BlockBuilder::default()
+        .header(header)
+        .uncles(uncles)
+        .transactions(txs)
+        .proposals(proposals)
+        .build()
+}
+
+fn build_uncle_block_view(block: &BlockTable) -> UncleBlockView {
+    UncleBlockBuilder::default()
+        .header(build_header_view(&block).data())
+        .proposals(build_proposals(&block.proposals.bytes))
+        .build()
+        .into_view()
+}
+
+fn build_header_view(block: &BlockTable) -> HeaderView {
+    HeaderBuilder::default()
+        .number(block.block_number.pack())
+        .parent_hash(
+            Byte32::from_slice(&block.parent_hash.bytes)
+                .expect("impossible: fail to pack parent_hash"),
+        )
+        .compact_target(block.compact_target.pack())
+        .nonce(utils::decode_nonce(&block.nonce.bytes).pack())
+        .timestamp(block.block_timestamp.pack())
+        .version((block.version as u32).pack())
+        .epoch(
+            EpochNumberWithFraction::new(
+                block.epoch_number,
+                block.epoch_block_index as u64,
+                block.epoch_length as u64,
+            )
+            .number()
+            .pack(),
+        )
+        .dao(Byte32::from_slice(&block.dao.bytes).expect("impossible: fail to pack dao"))
+        .transactions_root(
+            Byte32::from_slice(&block.transactions_root.bytes)
+                .expect("impossible: fail to pack transactions_root"),
+        )
+        .proposals_hash(
+            Byte32::from_slice(&block.proposals_hash.bytes)
+                .expect("impossible: fail to pack proposals_hash"),
+        )
+        .uncles_hash(
+            Byte32::from_slice(&block.uncles_hash.bytes)
+                .expect("impossible: fail to pack uncles_hash"),
+        )
+        .build()
+}
+
+// TODO: is possible?
+fn build_witness(_input: &[u8]) -> Vec<packed::Bytes> {
+    todo!()
+}
+
+fn build_proposals(_input: &[u8]) -> ProposalShortIdVec {
+    todo!()
+}
+
+fn build_header_deps(_input: &[u8]) -> Vec<Byte32> {
+    todo!()
+}
+
+fn build_cell_deps(_input: &[u8]) -> Vec<CellDep> {
+    todo!()
+}
+
+fn build_cell_inputs(input_cells: Option<&Vec<CellTable>>) -> Vec<CellInput> {
+    let cells = match input_cells {
+        Some(cells) => cells,
+        None => return vec![],
+    };
+    cells
+        .iter()
+        .map(|cell| {
+            let out_point = OutPointBuilder::default()
+                .tx_hash(
+                    Byte32::from_slice(&cell.tx_hash.bytes).expect("impossible: fail to pack sinc"),
+                )
+                .index((cell.output_index as u32).pack())
+                .build();
+            CellInputBuilder::default()
+                .since(cell.since.expect("impossible: fail to pack since").pack())
+                .previous_output(out_point)
+                .build()
+        })
+        .collect()
+}
+
+// TODO: lock and type scripts
+fn build_cell_outputs(cells: Option<&Vec<CellTable>>) -> Vec<CellOutput> {
+    let cells = match cells {
+        Some(cells) => cells,
+        None => return vec![],
+    };
+    cells
+        .iter()
+        .map(|cell| {
+            CellOutputBuilder::default()
+                .capacity(cell.capacity.pack())
+                .build()
+        })
+        .collect()
+}
+
+fn build_outputs_data(cells: Option<&Vec<CellTable>>) -> Vec<packed::Bytes> {
+    let cells = match cells {
+        Some(cells) => cells,
+        None => return vec![],
+    };
+    cells
+        .iter()
+        .map(|cell| Bytes::from(cell.data.bytes.clone()).pack())
+        .collect()
+}
+
+fn build_transaction_view(
+    version: u32,
+    witnesses: Vec<packed::Bytes>,
+    inputs: Vec<CellInput>,
+    outputs: Vec<CellOutput>,
+    outputs_data: Vec<packed::Bytes>,
+    cell_deps: Vec<CellDep>,
+    header_deps: Vec<packed::Byte32>,
+) -> TransactionView {
+    TransactionBuilder::default()
+        .version(version.pack())
+        .witnesses(witnesses)
+        .inputs(inputs)
+        .outputs(outputs)
+        .outputs_data(outputs_data)
+        .cell_deps(cell_deps)
+        .header_deps(header_deps)
+        .build()
 }
