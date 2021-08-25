485c29c445595b8268b2bd8266e819fcc92a6e15
KaoImin <kaoimin@qq.com>
2021-08-25 14:32:11 +0800

    feat: add process exit

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,21 @@
+use tokio::{sync::mpsc::channel, task};
+
+use std::{panic, process};
+
 #[tokio::main]
 async fn main() {
+    let (panic_sender, mut panic_receiver) = channel::<()>(1);
+
+    panic::set_hook(Box::new(move |_info: &panic::PanicInfo| {
+        panic_sender.try_send(()).expect("panic_receiver is droped");
+    }));
+
+    task::spawn_local(async move {
+        if let Some(_) = panic_receiver.recv().await {
+            process::exit(-1);
+        }
+    });
+
     let mercury = core_cli::Cli::init();
     mercury.start().await;
 }
