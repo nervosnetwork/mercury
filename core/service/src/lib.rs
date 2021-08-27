10772aed0e3d3a5ccd5746f087129c0abc53d317
KaoImin <kaoimin@qq.com>
2021-08-27 14:32:31 +0800

    cargo fmt

--- core/service/src/lib.rs
+++ core/service/src/lib.rs
@@ -185,7 +185,6 @@ impl Service {
     pub async fn do_sync(&self, batch_size: usize) -> Result<()> {
         let (local_tip, _) = self.store.get_tip().await?.unwrap_or_default();
         let chain_tip = self.ckb_client.get_tip_block_number().await?;
-        
 
         if chain_tip < local_tip + 50000 {
             return Ok(());
