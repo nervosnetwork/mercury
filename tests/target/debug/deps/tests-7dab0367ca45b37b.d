092090eb79b1c24485b301815a26688abc823173
wancencen <wancencen@cryptape.com>
2021-08-29 11:48:42 +0800

    rm tests/target

--- tests/target/debug/deps/tests-7dab0367ca45b37b.d
+++
@@ -1,5 +0,0 @@
-/Users/wcc/Documents/myWorkSpace/rust/mercury/tests/target/debug/deps/tests-7dab0367ca45b37b.rmeta: src/main.rs
-
-/Users/wcc/Documents/myWorkSpace/rust/mercury/tests/target/debug/deps/tests-7dab0367ca45b37b.d: src/main.rs
-
-src/main.rs:
