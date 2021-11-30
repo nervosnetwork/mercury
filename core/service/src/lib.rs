6808217e19746740012473d06a6b2c5db64624a6
KaoImin <kaoimin@qq.com>
2021-12-01 00:49:01 +0800

    fix(sql): change index type to btree

--- core/service/src/lib.rs
+++ core/service/src/lib.rs
@@ -159,7 +159,7 @@ impl Service {
 
         if (!sync_handler.is_previous_in_update().await?)
             && node_tip
-                .checked_sub(mercury_count)
+                .checked_sub(mercury_count - 1)
                 .ok_or_else(|| anyhow!("chain tip is less than db tip"))?
                 < 1000
         {
