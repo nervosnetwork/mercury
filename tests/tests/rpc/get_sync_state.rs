84041d7f7a4c4b74fa2ba889a2105caaccef8fcb
fjchen7 <fjchen7@gmail.com>
2021-12-22 10:46:05 +0800

    feat: Rewrite get_sync_state integration test cases in rust

---
+++ tests/tests/rpc/get_sync_state.rs
@@ -0,0 +1,21 @@
+use serde_json::Value;
+
+use super::common::post_http_request;
+
+#[test]
+fn test_sync_state() {
+    let resp = post_http_request(r#"{
+        "jsonrpc": "2.0",
+        "method": "get_sync_state",
+        "params": [],
+        "id": 100
+    }"#);
+    let r = &resp["result"];
+
+    // assert!(resp["error"] != Value::Null);
+
+
+    assert!(r["value"] != Value::Null);
+    assert!(r["value"]["current"].as_i64().unwrap() > 0);
+    assert!(r["value"]["target"].as_i64().unwrap() > 0);
+}
--- tests/tests/rpc/mod.rs
+++ tests/tests/rpc/mod.rs
@@ -2,3 +2,4 @@ pub mod common;
 mod get_db;
 mod get_mercury_info;
 mod get_block_info;
+mod get_sync_state;
