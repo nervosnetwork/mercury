9983680c5917434ba5214dc517c25275846acae3
KaoImin <kaoimin@qq.com>
2022-01-14 14:20:57 +0800

    fix

--- core/rpc/types/src/axon/mod.rs
+++ core/rpc/types/src/axon/mod.rs
@@ -20,7 +20,7 @@ impl TryFrom<Identity> for crate::Identity {
     type Error = String;
 
     fn try_from(id: Identity) -> Result<Self, Self::Error> {
-        if id.content.len() != 40 {
+        if id.content.len() != 42 {
             return Err(String::from("Invalid Admin Identity"));
         }
 
