ca8692e313c945376d8e2a79f8ba22158856a712
EthanYuan <yuanchengxing@hotmail.com>
2022-08-19 12:01:30 +0800

    add unit test for get_cells.

--- core/rpc/core/src/tests/rpc_test.rs
+++ core/rpc/core/src/tests/rpc_test.rs
@@ -1,17 +1,22 @@
 use super::*;
 use crate::r#impl::utils;
 
-use ckb_jsonrpc_types::OutPoint;
-use ckb_types::packed;
+use ckb_jsonrpc_types::{JsonBytes, OutPoint, Script, ScriptHashType};
+use ckb_types::packed::{self, Uint16, Uint64};
 use common::lazy::SECP256K1_CODE_HASH;
 use common::{Address, DetailedCell, NetworkType, Order, PaginationRequest, Range};
+use core_rpc_types::indexer::{ScriptType, SearchKey};
 use core_rpc_types::lazy::{CURRENT_BLOCK_NUMBER, CURRENT_EPOCH_NUMBER};
+use core_rpc_types::uints::JsonUint;
 use core_rpc_types::{
     indexer, AssetInfo, AssetType, Balance, DaoClaimPayload, DaoWithdrawPayload, ExtraType,
     GetBalancePayload, GetBlockInfoPayload, Identity, IdentityFlag, Item, JsonItem, Record,
     SinceConfig, SinceFlag, SinceType, ToInfo, TransactionInfo,
 };
 use tokio::test;
+use xsql_test::read_block_view;
+
+const BLOCK_DIR: &str = "../../../devtools/test_data/blocks/";
 
 async fn new_rpc(network: NetworkType, url: &str) -> MercuryRpcImpl<CkbRpcClient> {
     let engine = RpcTestEngine::new_pg(network, url).await;
@@ -76,4 +81,60 @@ async fn test_get_live_cells_by_item() {
         .await
         .unwrap();
     print_cells(&rpc, cells);
+}
+
+#[test]
+async fn test_indexer_get_cells() {
+    let engine = RpcTestEngine::new().await;
+    let rpc = engine.rpc(NetworkType::Dev);
+
+    for i in 0..10 {
+        engine
+            .store
+            .append_block(read_block_view(i, String::from(BLOCK_DIR).clone()).into())
+            .await
+            .unwrap();
+    }
+
+    let script = Script {
+        code_hash: H256::default(),
+        hash_type: ScriptHashType::Data,
+        args: JsonBytes::from_vec(Vec::new()),
+    };
+
+    // with_data: Some(false)
+    let search_key = SearchKey {
+        script: script.clone(),
+        script_type: ScriptType::Lock,
+        filter: None,
+        with_data: Some(false),
+    };
+    let cells = rpc
+        .get_cells(search_key, Order::Asc, JsonUint::from(7u16), None)
+        .await
+        .unwrap()
+        .objects;
+    assert_eq!(7, cells.len());
+    for cell in cells {
+        assert!(cell.output_data.is_none());
+        assert_eq!(0, cell.block_number.value());
+    }
+
+    // with_data: None
+    let search_key = SearchKey {
+        script,
+        script_type: ScriptType::Lock,
+        filter: None,
+        with_data: None,
+    };
+    let cells = rpc
+        .get_cells(search_key, Order::Asc, JsonUint::from(7u16), None)
+        .await
+        .unwrap()
+        .objects;
+    assert_eq!(7, cells.len());
+    for cell in cells {
+        assert!(cell.output_data.is_some());
+        assert_eq!(0, cell.block_number.value());
+    }
 }
