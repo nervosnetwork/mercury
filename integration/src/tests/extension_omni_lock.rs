b6723a0352be434b415a21d37ae2545ca5fb252b
EthanYuan <yuanchengxing@hotmail.com>
2022-10-18 21:27:16 +0800

    add test_get_balance_omni_ethereum

--- integration/src/tests/extension_omni_lock.rs
+++ integration/src/tests/extension_omni_lock.rs
@@ -1,45 +1,35 @@
-use crate::const_definition::SIGHASH_TYPE_HASH;
-use crate::utils::address::generate_rand_private_key;
+use super::IntegrationTest;
+use crate::const_definition::MERCURY_URI;
+use crate::utils::address::omni_lock::prepare_omni_lock_address_with_capacity;
 
-use common::{Address, AddressPayload, NetworkType};
+use crate::utils::rpc_client::MercuryRpcClient;
 
-use anyhow::Result;
-use ckb_hash::blake2b_256;
-use ckb_jsonrpc_types::OutPoint;
-use ckb_types::H256;
-use ckb_types::{bytes::Bytes, core::ScriptHashType, packed, prelude::*};
+use core_rpc_types::{AssetInfo, AssetType, GetBalancePayload, JsonItem};
 
-use std::str::FromStr;
+use std::collections::HashSet;
 
-pub(crate) fn _prepare_omni_lock_address_with_capacity(
-    _capacity: u64,
-) -> Result<(Address, H256, OutPoint)> {
-    todo!()
-}
+inventory::submit!(IntegrationTest {
+    name: "test_get_balance_omni_ethereum",
+    test_fn: test_get_balance_omni_ethereum
+});
+fn test_get_balance_omni_ethereum() {
+    let (address, address_pk, _) =
+        prepare_omni_lock_address_with_capacity(300_0000_0000).expect("prepare ckb");
+    let _pks = vec![address_pk];
+    let mercury_client = MercuryRpcClient::new(MERCURY_URI.to_string());
 
-pub(crate) fn _generate_omni_lock_address_pk_pair(
-    _auth: Bytes,
-    _omni_args: Bytes,
-) -> (Address, H256) {
-    // generate pubkey by privkey
-    let pk = generate_rand_private_key();
-    let secret_key = secp256k1::SecretKey::from_str(&pk.to_string())
-        .expect("impossible: fail to build secret key");
-    let secp256k1: secp256k1::Secp256k1<secp256k1::All> = secp256k1::Secp256k1::new();
-    let pubkey = secp256k1::PublicKey::from_secret_key(&secp256k1, &secret_key);
-
-    // pubkey hash
-    let pubkey = &pubkey.serialize()[..];
-    let pubkey_hash = blake2b_256(pubkey);
-
-    // generate args by pubkey hash
-    let args = Bytes::from(pubkey_hash[0..20].to_vec());
-
-    // secp address
-    let secp_code_hash =
-        packed::Byte32::from_slice(SIGHASH_TYPE_HASH.as_bytes()).expect("impossible:");
-    let payload = AddressPayload::new_full(ScriptHashType::Type, secp_code_hash, args);
-    let address = Address::new(NetworkType::Testnet, payload, true);
-
-    (address, pk)
+    let mut asset_infos = HashSet::new();
+    asset_infos.insert(AssetInfo::new_ckb());
+    let balance_payload = GetBalancePayload {
+        item: JsonItem::Address(address.to_string()),
+        asset_infos,
+        extra: None,
+        tip_block_number: None,
+    };
+    let balance = mercury_client.get_balance(balance_payload).unwrap();
+    assert_eq!(balance.balances.len(), 1);
+    assert_eq!(balance.balances[0].asset_info.asset_type, AssetType::CKB);
+    assert_eq!(balance.balances[0].occupied, 0u128.into());
+    assert_eq!(balance.balances[0].free, 300_0000_0000u128.into());
+    assert_eq!(balance.balances[0].frozen, 0u128.into());
 }
